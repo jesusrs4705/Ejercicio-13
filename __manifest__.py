5f0a1a1a3ad052dd5af7cf5accf143bf3a89af94
jesusrs4705 <yisusrs2005@gmail.com>
2025-04-20 12:38:53 +0200

    Ejercicio 13

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Partner Company Security',
+    'version': '1.0',
+    'author': 'Jesus',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'application': True,
+    'license': 'LGPL-3',
+}
---
+++ models/res_partner.py
@@ -0,0 +1,20 @@
+from odoo import models, api, _
+from odoo.exceptions import AccessError
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    @api.model
+    def write(self, vals):
+        # Verificar si el partner está vinculado a una compañía
+        company = self.env['res.company'].search([('partner_id', 'in', self.ids)], limit=1)
+        
+        if company:
+            # Verificar si el usuario actual tiene el permiso de Administración/Configuración
+            if not self.env.user.has_group('base.group_system'):
+                raise AccessError(_(
+                    'No tiene permisos para modificar el contacto asociado a la compañía. '
+                    'Solo usuarios con permisos de Administración/Configuración pueden realizar esta acción.'
+                ))
+        
+        return super(ResPartner, self).write(vals)
